5542216e46fdfc45874a78b9331e1765997ebba3
l-bin <l.binacchi@gmail.com>
2021-04-15 12:29:59 +0200

    add read function

--- LB_PIC16F887_Signal_Generator/src/KEYPAD_PCA9554.s
+++ LB_PIC16F887_Signal_Generator/src/KEYPAD_PCA9554.s
@@ -9,6 +9,7 @@
 ; Functions
 ; =========
 global	INIT_PCA9554	    ; Initializes the PCA9554 I/0 expander initialization
+global	READ_PCA9554	    ; Reads the PCA9554 in the KEYPAD_DATA
 
 
 ; Variables
@@ -23,7 +24,6 @@ KEYPAD_DATA:	DS 1
 PSECT code
 
 INIT_PCA9554:
-
     ; PORT -> input
     call    START_I2C
     movlw   PCA9554_KEYPAD_ADD_W
@@ -32,6 +32,23 @@ INIT_PCA9554:
     call    SEND_I2C
     movlw   0xFF
     call    SEND_I2C
+    call    STOP_I2C
+
+    return
+
+READ_PCA9554:
+    ; PORT -> input
+    call    START_I2C
+    movlw   PCA9554_KEYPAD_ADD_W
+    call    SEND_I2C
+    movlw   0x00
+    call    SEND_I2C
+    call    RESTART_I2C
+    movlw   PCA9554_KEYPAD_ADD_R
+    call    SEND_I2C
+    call    WAIT_ACK_I2C
+    call    RECEIVE_I2C
+    movwf   KEYPAD_DATA
     call    STOP_I2C
 
     return
