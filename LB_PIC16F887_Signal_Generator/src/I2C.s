e40156d62b9af12d8df78f9455dc3bf9051e867f
l-bin <l.binacchi@gmail.com>
2021-04-15 12:28:35 +0200

    add receive, restart and wait ack functions

--- LB_PIC16F887_Signal_Generator/src/I2C.s
+++ LB_PIC16F887_Signal_Generator/src/I2C.s
@@ -6,8 +6,11 @@
 ; =========
 global	INIT_I2C	; I2C bus initialization
 global	START_I2C	; Sends the I2C start condition
+global	RESTART_I2C	; Repeats start condition
 global	STOP_I2C	; Sends the I2C stop condition
-global	SEND_I2C	; Sends data using I2C bus
+global	SEND_I2C	; Sends data using the I2C bus
+global	RECEIVE_I2C	; Receives data using the I2C bus
+global	WAIT_ACK_I2C	; Waits for the I2C acknowledgement
 global	IDLE_I2C	; Waits for the I2C bus to be available
 
 
@@ -64,6 +67,26 @@ START_I2C:
     return
 
 
+RESTART_I2C:
+    ; Wait for the I2C to be available
+    call    IDLE_I2C
+
+    ; Clear the interrupt flag
+    banksel PIR1
+    bcf	    SSPIF
+
+    ; Set the start condition
+    banksel SSPCON2
+    bsf	    RSEN
+
+    ; Wait for the end of the transmission
+    banksel PIR1
+    btfss   SSPIF
+    goto    $-1
+
+    return
+
+
 STOP_I2C:
     ; Wait for the I2C to be available
     call    IDLE_I2C
@@ -103,6 +126,33 @@ SEND_I2C:
     return
 
 
+RECEIVE_I2C:
+    ; Wait for the I2C to be available
+    call    IDLE_I2C
+
+    ; Set the 
+    banksel SSPCON2
+    bsf	    RCEN
+
+    ; Wait for the end of the transmission
+    btfsc   RCEN
+    goto    $-1
+
+    ; Place the received data in the W register
+    banksel SSPBUF
+    movf    SSPBUF, w
+
+    return
+
+
+WAIT_ACK_I2C:
+    call    IDLE_I2C
+    btfsc   ACKSTAT
+    goto    $-1
+
+    return
+
+
 IDLE_I2C:
     banksel SSPCON2
 IDLE:
@@ -114,8 +164,9 @@ IDLE:
     goto    IDLE
     btfsc   PEN		; Stop condition enabled
     goto    IDLE
-    btfsc   RCEN	; Receive enabled (not used here)
+    btfsc   RCEN	; Receive enabled
     goto    IDLE
-    btfsc   ACKEN	; Acknowledgement sequence enabled (not used here)
+    btfsc   ACKEN	; Acknowledgement sequence enabled
     goto    IDLE
+
     return
